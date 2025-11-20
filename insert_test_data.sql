9af0240b9071e9909acfc440d5aa5fef56f68f66
“Michal <Michas1827@gmail.com>
2025-11-20 22:19:32 +0000

    fixed hash again...

--- insert_test_data.sql
+++ insert_test_data.sql
@@ -10,4 +10,4 @@ VALUES ('Brighton Rock', 20.25),
 -- Insert a default user 'gold' with password 'smiths'
 -- The password_hash below is bcrypt-hashed version of 'smiths'
 INSERT INTO users (username, first_name, last_name, email, password_hash)
-VALUES ('gold', 'Gold', 'User', 'gold@example.com', '$2b$10$hnHlyzwTyURiVWASGixfb.27aahcF3UDqe1jHiyCI5s5fNlxWEQSms');
+VALUES ('gold', 'Gold', 'User', 'gold@example.com', '$2b$10$hnHlyzwTyURiVWASGixfb.27aahcF3UDqe1jHiyCI5s5fNlxWEQSm');
